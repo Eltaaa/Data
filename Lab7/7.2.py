ce3547f82c8a95d3b1be1d927bd0d7131baeef36
Eltaa <orcapeerawat@gmail.com>
2023-03-23 20:56:36 +0700

    7.2

---
+++ Lab7/7.2.py
@@ -0,0 +1,58 @@
+import random
+from time import time
+population = [x for x in range(1_000_000)]
+
+
+def randomList(size):
+    """ generate random list"""
+    # x = random.sample(population, length)
+    # print(x)
+    # return x
+    return random.sample(population, size)
+
+
+def O2isIntersect(a, b, c):
+    # x = set(a)
+    # y = set(b)
+    # z = set(c)
+
+    for i in a:  # N
+        for j in b:  # N
+
+            if i == j:  # + smth
+                for k in c:
+                    if i == k:
+                        return True
+
+    return False
+
+
+def O3isIntersect(a, b, c):
+
+    for i in a:  # N
+        for j in b:  # N
+            for k in c:  # N
+
+                # print(".", end=" ")
+                if i == j == k:
+                    return True
+
+    # N**3 + N
+    return False
+
+
+def main(size):
+    a = randomList(size)
+    b = randomList(size)
+    c = randomList(size)
+
+    s = time()
+    print(O2isIntersect(a, b, c))
+    e = time()
+    print("O2 Elapse :", (e - s))
+
+    s = time()
+    print(O3isIntersect(a, b, c))
+    e = time()
+    print("O3 Elapse :", (e - s))
+
---
+++ Lab7/test.py
@@ -0,0 +1,6 @@
+a = [7, 6, 5, 1, 2, 3]
+
+# a.sort()
+sorted(a)
+
+print(a)
